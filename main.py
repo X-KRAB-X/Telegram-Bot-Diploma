c1c412ce96b7215090748d88eed73d6302de5135
Andrey Bokarev <gagamen2004@gmail.com>
2023-10-07 12:25:50 +0300

    some changes

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 """ Здесь происходит только запуск бота. """
 
-from handlers import *
-from telebot import custom_filters
-
+from loader import bot
+from utils.set_commands import set_commands
+import handlers
 
 if __name__ == '__main__':
-    my_bot.add_custom_filter(custom_filters.StateFilter(my_bot))
-    my_bot.infinity_polling()
-
-# База данных содержит:
-# ('Кросовки', 25, 3000)
-# ('Кепки', 10, 700)
-# ('Шорты', 13, 1600)
-# ('Джинсы', 18, 2500)
-# ('Футболки', 20, 1000)
-# ('Носки', 30, 500)
-# ('Толстовки', 16, 2750)
+    set_commands(bot)
+    bot.infinity_polling()
