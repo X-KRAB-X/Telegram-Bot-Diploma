6324236aa5bbc625e784ac9a41859c3c308f61ef
Andrey Bokarev <gagamen2004@gmail.com>
2023-10-10 10:48:56 +0300

    changed docs and message to user

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,6 +1,10 @@
+"""Команда для запуска. Бот здоровается и описывает себя."""
+
 from loader import bot
+
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
-    """ Простое приветствие. """
-    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!')
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\n'
+                                      f'Меня зовут Лой. Я привязанный к складу бот.\n'
+                                      f'Чтобы узнать мои команды, напишите /help')
