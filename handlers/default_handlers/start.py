20ea197ea90b0dc785af59adee0495a7f3603b73
Andrey Bokarev <gagamen2004@gmail.com>
2023-11-01 22:03:56 +0300

    now saving request to database

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,6 +1,7 @@
 """Команда для запуска. Бот здоровается и описывает себя."""
 
 from loader import bot
+from datadase.history_saving import save_req
 
 
 @bot.message_handler(commands=['start'])
@@ -8,3 +9,4 @@ def start_bot(message):
     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}!\n'
                                       f'Меня зовут Лой. Я привязанный к складу бот.\n'
                                       f'Чтобы узнать мои команды, напишите /help')
+    save_req('start')
