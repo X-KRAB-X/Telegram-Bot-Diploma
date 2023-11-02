6d52bc9b5faf1d337a8c301f1fdfc90ae5531cdf
Andrey Bokarev <gagamen2004@gmail.com>
2023-11-02 17:04:44 +0300

    new function, docs changed

--- datadase/history_saving.py
+++ datadase/history_saving.py
@@ -1,7 +1,9 @@
-"""Файл для сохранения команды в базе данных"""
+""" Файл для работы с базой данных. """
+
 
 import os
 from peewee import SqliteDatabase, Model, CharField
+import sqlite3
 
 
 path = os.path.abspath(os.path.join('datadase', 'history.db'))  # работает только из файла main, иначе путь ломается.
@@ -9,6 +11,11 @@ db = SqliteDatabase(path)
 
 
 class Requests(Model):
+    """
+    Модель для базы данных.
+    Хранит в себе id и саму команду.
+    """
+
     command = CharField()
 
     class Meta:
@@ -19,5 +26,20 @@ db.create_tables([Requests])
 
 
 def save_req(command):
+    """
+    Функция для создания записи в базе данных.
+    """
+
     req = Requests(command=command)
     req.save()
+
+
+def last_ten():
+    """
+    Функция для получения списка, состоящего из 10 последних записей.
+    """
+
+    with sqlite3.connect(path) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM (SELECT * FROM requests ORDER BY id DESC LIMIT 10) t ORDER BY id')
+        return cursor.fetchall()
